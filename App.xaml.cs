759fbbee4a2a284113bf2e7b2e204fc6cd42e8c7
gnanabommineni <gnana.bommineni@gmail.com>
2026-07-28 14:14:39 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace nApps.Futs.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "nApps.Futs.Mobile" };
+    }
+}
